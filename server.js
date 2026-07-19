1c0647c4b31977c7768fe9fb17e1d6001e726dec
Rahul Tiwari <t.rahul3105@gmail.com>
2026-07-19 20:52:22 +0530

    Stage 1: Environment variables

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from "express";
+import dotenv from 'dotenv'
 import taskRoute from "./routes/taskRoutes.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./swagger/swagger.js";
 
 const app = express();
 const port = 3000;
+dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
